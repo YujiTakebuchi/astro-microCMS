a145908e82f0a44cf1f4eed975b8f582758d19b4
YujiTakebuchi <takeyou.swift@gmail.com>
2023-02-05 23:31:27 +0900

    記事検索表示実装

--- src/components/BlogSearch.tsx
+++ src/components/BlogSearch.tsx
@@ -6,15 +6,37 @@ const BlogSearch = () => {
   const q = params.get("q");
 
   console.log(q);
-  const { data } = useSWR(q === null ? null : ["/search", q], ([, q]) =>
-    getBlogs({
-      fields: ["id", "data"],
-      q,
-    })
+  const { data, error, isLoading } = useSWR(
+    q === null ? null : ["/search", q],
+    ([, q]) =>
+      getBlogs({
+        fields: ["id", "title"],
+        q,
+      })
   );
   console.log(data);
 
-  return <div></div>;
+  if (error) <div>エラーが発生しました…！</div>;
+
+  if (isLoading) <div>読み込み中…ちょっと待っててくれ</div>;
+
+  return (
+    <div>
+      {data?.contents.length !== 0 ? (
+        <ul>
+          {data?.contents.map(({ id, title }) => {
+            return (
+              <li key={id}>
+                <a href={id}>{title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div>ありませんでした、すみません。</div>
+      )}
+    </div>
+  );
 };
 
 export default BlogSearch;
