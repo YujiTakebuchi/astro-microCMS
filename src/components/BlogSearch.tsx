ee0f4caa731fe1c650454043cd211670773d5d80
YujiTakebuchi <takeyou.swift@gmail.com>
2023-02-05 23:22:39 +0900

    ID, タイトルでの検索機能のベース作成

--- src/components/BlogSearch.tsx
+++ src/components/BlogSearch.tsx
@@ -1,8 +1,18 @@
+import useSWR from "swr";
+import { getBlogs } from "../library/microcms";
+
 const BlogSearch = () => {
   const params = new URLSearchParams(window.location.search);
   const q = params.get("q");
 
   console.log(q);
+  const { data } = useSWR(q === null ? null : ["/search", q], ([, q]) =>
+    getBlogs({
+      fields: ["id", "data"],
+      q,
+    })
+  );
+  console.log(data);
 
   return <div></div>;
 };
